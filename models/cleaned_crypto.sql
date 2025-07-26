db7d1ef0a7953ea4b06661c0e425c4dda0f18470
Prarabdh Bhatia <prarabdhbhatia24@gmail.com>
2025-07-26 02:07:29 +0000

    commit

---
+++ models/cleaned_crypto.sql
@@ -0,0 +1,21 @@
+\SELECT
+  coin_id,
+  symbol,
+  price_usd,
+  market_cap_usd,
+  percent_change_24h,
+  volume_24h,
+  price_change_24h,
+  high_24h,
+  low_24h,
+  ath,
+  atl,
+  circulating_supply,
+  total_supply,
+  max_supply,
+  last_updated,
+  TIMESTAMP_TRUNC(last_updated, DAY) AS snapshot_date
+FROM
+  `n8nauth-465703.crypto_dataset.prices`
+WHERE
+  price_usd IS NOT NULL
